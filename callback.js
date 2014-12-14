d379f252e9856b954aad33308fdad4062bf8700d
alfred sang <i5ting@126.com>
2014-12-14 23:31:09 +0800

    fix demo expression

--- callback.js
+++ callback.js
@@ -1,1 +1,15 @@
 console.log("i am callback.js. process.argv是: " + process.argv);
+
+var request = require('request');
+
+request.post({
+	url:'http://at35.com:4567/pub', 
+	form: {
+		key:'foo',
+		value:'' + str
+	}
+}, function(err,httpResponse,body){ 
+	/* ... */ 
+	if(err)
+		console.log(err);
+})	
