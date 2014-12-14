5a4e00a5c77da51d0daef2a8f2a2d44b8b1a243e
alfred sang <i5ting@126.com>
2014-12-14 22:38:55 +0800

    init with example

--- example.js
+++ example.js
@@ -2,6 +2,21 @@ var file = 'file.log'
 var search_content_express = /hadoop/g
 var done = function(str,res){
 	console.log("----"+str);
+	var request = require('request');
+	setTimeout(function(){
+		request.post({
+			url:'http://at35.com:4567/pub', 
+			form: {
+				key:'foo',
+				value:'' + str
+			}
+		}, function(err,httpResponse,body){ 
+			/* ... */ 
+			if(err)
+				console.log(err);
+		})	
+	},0);
+	
 }
 	
 var verbose = true;
